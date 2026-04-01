0103d307fe75d1c741f9e4a4635867d8ec528ee7
Viktoriia <savchukv2006@gmail.com>
2026-04-01 14:43:04 +0200

    Initial commit

---
+++ Perceptron/Perceptron.swift
@@ -0,0 +1,16 @@
+//
+//  Perceptron.swift
+//  Perceptron
+//
+//  Created by Viktoriia Savchuk  on 01/04/2026.
+//
+
+import Foundation
+
+struct Perceptron {
+    var weights: [Double] = [0, 0, 0, 0]
+    var threshold: Double = 0
+    var parameterAlfa: Double = 0.1
+    
+    
+}
---
+++ Perceptron/ProgramManager.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Perceptron
+//
+//  Created by Viktoriia Savchuk  on 01/04/2026.
+//
+
+import Foundation
+
+struct ProgramManager {
+    
+    
+}
